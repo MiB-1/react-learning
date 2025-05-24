05bb42c2b366fa7ce629c333f92764145494c06e
vignesh🥷🏻 <vignesh.krishnasamy2@ing.com>
2025-05-24 22:01:18 +0200

    complete version 1

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -10,11 +10,11 @@ function Contact() {
             <div className="contact-wrapper">
                 <div className="contact-title">Get in touch</div>
             </div>
-            <p className="contact-description">
+            <div className="contact-description">
                 <p className="description-text">
                     What's next? Feel free to reach out to me if you're looking for a developer, have a query, or simply want to connect.
                 </p>
-            </p>
+            </div>
             <div className="contact-info">
                 <a href="mailto:vignesh134410@gmail.com" className="mail-info">
                     <img className="contact-img" src={`/${theme}-mail.svg`} alt="mail"/>
--- src/navigation-button/NavigationButton.jsx
+++ src/navigation-button/NavigationButton.jsx
@@ -1,10 +1,69 @@
 import ThemeToggleButton from "../ThemeComponents/ThemeToggleButton.jsx";
 import './NavigationButton.css';
-import {contactRef, homeRef, skillRef, workRef} from "../App.jsx";
+import {ThemeContext} from "../ThemeComponents/ThemeContext.jsx";
+import { contactRef, homeRef, skillRef, workRef } from "../App.jsx";
+import { useContext, useEffect, useState } from "react";
 
 function NavigationButton({ callback }) {
+
+    const [isMobile, setIsMobile] = useState( window.innerWidth < 767);
+    const [isOpen, setIsOpen] = useState(false);
+    const { theme } = useContext(ThemeContext);
+
+
+    useEffect(() => {
+        const resizeHandler = () => {
+            setIsMobile(window.innerWidth < 767);
+        }
+
+        window.addEventListener('resize', resizeHandler);
+        return () => window.removeEventListener('resize', resizeHandler);
+    }, []);
+
+    const pointerCallBack = (reference) => {
+        setIsOpen(false);
+        callback(reference);
+    }
+
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+
+        return () => {
+            document.body.style.overflow = '';
+        };
+    }, [isOpen]);
+
+    if(isMobile) {
+        return (
+            <nav className="navigation-group">
+                <div onClick={() => setIsOpen(true)}>
+                    <img id="ham-burger" src={`${theme}-bars.svg`} alt="Menu"/>
+                </div>
+
+                <div className={`menu-overlay ${isOpen ? 'open' : ''}`}>
+                    <div className="menu-content">
+                        <div id="close-wrapper" onClick={() => setIsOpen(false)}>
+                            <img id="close-button" src={`${theme}-close.svg`} alt="Close button"/>
+                        </div>
+                        <div className="navigation-group">
+                            <button onClick={() => pointerCallBack(homeRef)} type="button" className="navigation-button">Home</button>
+                            <button onClick={() => pointerCallBack(skillRef)} type="button" className="navigation-button">Skills</button>
+                            <button onClick={() => pointerCallBack(workRef)} type="button" className="navigation-button">Work</button>
+                            <button onClick={() => pointerCallBack(contactRef)} type="button" className="navigation-button">Contact</button>
+                            <ThemeToggleButton />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
     return (
-        <div className="navigation-group">
+        <nav className="navigation-group">
             <button onClick={() => callback(homeRef)} type="button" className="navigation-button">Home</button>
             <button onClick={() => callback(skillRef)} type="button" className="navigation-button">Skills</button>
             <button onClick={() => callback(workRef)} type="button" className="navigation-button">Work</button>
@@ -12,7 +71,7 @@ function NavigationButton({ callback }) {
             <div className="divider"></div>
             <ThemeToggleButton/>
             {/*<button type="button" className="download-cv">Download CV</button>*/}
-        </div>
+        </nav>
     )
 }
 
